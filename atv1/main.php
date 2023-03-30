2bf682b07bc1d864716e1de80341827ad2be8e6d
Víctor Duplat Tigre <98099656+tigrevictorduplat@users.noreply.github.com>
2023-03-30 13:43:01 -0300

    Index, Subpages and Folders - Created

---
+++ atv1/main.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../styles/mainstyle.css">
+    <title>Atividade I - HTML BÁSICO</title>
+</head>
+<body background="../src/matrix-background.jpg">
+<header class="nav-bar">
+  <a href="https://github.com/tigrevictorduplat" target="_blank">
+    <img src="https://avatars.githubusercontent.com/u/98099656?v=4" alt="" class="mygithub-logo">
+  </a>
+</header>
+<main class="container">
+  <h1 class="page-title">
+    Meus Projetos
+  </h1>
+  <div class="link-list">
+  <ul>
+    <li><a href="questao1.html"><h3>Questão 01</h3></a></li>
+    <li><a href="questao2.html"><h3>Questão 02</h3></a></li>
+    <li><a href="questao3.html"><h3>Questão 03</h3></a></li>
+  </ul>
+  </div>
+  
+</main>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/mainstyle.css">
+    <title>Tiger Landing Page</title>
+</head>
+<body background="src/matrix-background.jpg">
+<header class="nav-bar">
+  <a href="https://github.com/tigrevictorduplat" target="_blank">
+    <img src="https://avatars.githubusercontent.com/u/98099656?v=4" alt="" class="mygithub-logo">
+  </a>
+</header>
+<main class="container">
+  <h1 class="page-title">
+    Meus Projetos
+  </h1>
+  <div class="link-list">
+  <ul>
+    <li><a href="atv1/main.php"><h3>Atividade 01 - HTML BÁSICO</h3></a></li>
+    <li><h3>Atividade 02 - IMAGENS E LINKS</h3></li>
+    <li><h3>Atividade 03 - TABELAS</h3></li>
+  </ul>
+  </div>
+  
+</main>
+</body>
+</html>
