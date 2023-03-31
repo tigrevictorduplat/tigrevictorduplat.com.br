ffc951d041f2ed731602b2b6b8f8620ebccb1e32
Víctor Duplat Tigre <98099656+tigrevictorduplat@users.noreply.github.com>
2023-03-31 14:10:51 -0300

    Lista I - Terminada

--- index.php
+++ index.php
@@ -5,24 +5,30 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styles/mainstyle.css">
+    <link rel="shortcut icon" href="src/morpheus.ico" type="image/x-icon">
     <title>Tiger Landing Page</title>
 </head>
 <body background="src/matrix-background.jpg">
 <header class="nav-bar">
-  <a href="https://github.com/tigrevictorduplat" target="_blank">
-    <img src="https://avatars.githubusercontent.com/u/98099656?v=4" alt="" class="mygithub-logo">
+  <a href="https://github.com/tigrevictorduplat" title="@tigrevictorduplat" target="_blank">
+    <img src="https://avatars.githubusercontent.com/u/98099656?v=4" alt="Octocat Icon" class="mygithub-logo">
   </a>
+  <a href="https://www.flaticon.com/free-icons/matrix" title="matrix icons" target="_blank">
+    <img src="src/morpheus.png" alt="Morpheus Icon" class="title-icon">
+</a>
 </header>
 <main class="container">
   <h1 class="page-title">
     Meus Projetos
   </h1>
   <div class="link-list">
-  <ul>
-    <li><a href="atv1/main.php"><h3>Atividade 01 - HTML BÁSICO</h3></a></li>
-    <li><h3>Atividade 02 - IMAGENS E LINKS</h3></li>
-    <li><h3>Atividade 03 - TABELAS</h3></li>
-  </ul>
+    <h3>Atividade 01 - HTML BÁSICO</h3>
+    <a href="lista1/lista1.php"><p>Lista I</p></a>
+    <a href="lista2/lista2.php"><p>Lista II</p></a>
+    <h3>Atividade 02 - IMAGENS E LINKS</h3>
+    <a href=""><p>Lista III</p></a>
+    <h3>Atividade 03 - TABELAS</h3></li>
+    <a href=""><p>Lista IV</p></a>
   </div>
   
 </main>
--- lista2/lista2.php
+++ lista2/lista2.php
@@ -5,26 +5,26 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../styles/mainstyle.css">
-    <title>Atividade I - HTML BÁSICO</title>
+    <link rel="shortcut icon" href="../src/morpheus.ico" type="image/x-icon">
+    <title>Lista II</title>
 </head>
 <body background="../src/matrix-background.jpg">
 <header class="nav-bar">
-  <a href="https://github.com/tigrevictorduplat" target="_blank">
+  <a href="https://github.com/tigrevictorduplat" title="@tigrevictorduplat" target="_blank">
     <img src="https://avatars.githubusercontent.com/u/98099656?v=4" alt="" class="mygithub-logo">
   </a>
+  <a href="https://www.flaticon.com/free-icons/matrix" title="matrix icons" target="_blank">
+    <img src="../src/morpheus.png" alt="Morpheus Icon" class="title-icon">
+</a>
 </header>
 <main class="container">
   <h1 class="page-title">
     Meus Projetos
   </h1>
   <div class="link-list">
-  <ul>
-    <li><a href="questao1.html"><h3>Questão 01</h3></a></li>
-    <li><a href="questao2.html"><h3>Questão 02</h3></a></li>
-    <li><a href="questao3.html"><h3>Questão 03</h3></a></li>
-  </ul>
+    <a href="copa.html"><h3>Acessar Página - História da Copa do Mundo</h3></a>
+    <a href="../index.php"><h3>Voltar ao início</h3></a>
   </div>
-  
 </main>
 </body>
 </html>
