d2ce817691c462cde3a7e11822c7852f9dea5e68
Víctor Duplat Tigre <98099656+tigrevictorduplat@users.noreply.github.com>
2023-04-15 14:53:48 -0300

    Lista 4 - Questão 2 (Finalizada)
    Updates na organização da pasta /src

--- lista1/lista1.php
+++ lista1/lista1.php
@@ -5,16 +5,16 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../styles/mainstyle.css">
-    <link rel="shortcut icon" href="../src/morpheus.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="../src/sumarios/morpheus.ico" type="image/x-icon">
     <title>Lista I</title>
 </head>
-<body background="../src/matrix-background.jpg">
+<body background="../src/sumarios/matrix-background.jpg">
 <header class="nav-bar">
   <a href="https://github.com/tigrevictorduplat" title="@tigrevictorduplat" target="_blank">
     <img src="https://avatars.githubusercontent.com/u/98099656?v=4" alt="" class="mygithub-logo">
   </a>
   <a href="https://www.flaticon.com/free-icons/matrix" title="matrix icons" target="_blank">
-    <img src="../src/morpheus.png" alt="Morpheus Icon" class="title-icon">
+    <img src="../src/sumarios/morpheus.png" alt="Morpheus Icon" class="title-icon">
 </a>
 </header>
 <main class="container">
