cfbfaaa18f1a9538e37625494917776c00b256af
aluno <aluno@INTRAUECA.CEFET.COM>
2023-04-20 10:27:56 -0300

    Lista 5 - Questão 1 (Finalizada)

---
+++ lista5/lista5.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../styles/mainstyle.css">
+    <link rel="shortcut icon" href="../src/sumarios/morpheus.ico" type="image/x-icon">
+    <title>Lista V</title>
+</head>
+<body background="../src/sumarios/matrix-background.jpg">
+<header class="nav-bar">
+  <a href="https://github.com/tigrevictorduplat" title="@tigrevictorduplat" target="_blank">
+    <img src="https://avatars.githubusercontent.com/u/98099656?v=4" alt="" class="mygithub-logo">
+  </a>
+  <a href="https://www.flaticon.com/free-icons/matrix" title="matrix icons" target="_blank">
+    <img src="../src/sumarios/morpheus.png" alt="Morpheus Icon" class="title-icon">
+</a>
+</header>
+<main class="container">
+  <h1 class="page-title">
+    Meus Projetos
+  </h1>
+  <div class="link-list">
+    <a href="questao1.html"><h3>Questão 01 - Form - Comentário em Blog</h3></a>
+    <a href="questao2.html"><h3>Questão 02 - Form - Cadastro Universidade </h3></a>
+    <a href="questao3.html"><h3>Questão 03 - Form - Cadastro</h3></a>
+    <a href="../index.php"><h3>Voltar ao início</h3></a>
+  </div>
+</main>
+</body>
+</html>
